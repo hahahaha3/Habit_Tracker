28bb5bd540c908dae51d33366dc065a662c204ed
hahahaha3 <eksql2689@gmail.com>
2022-02-03 23:38:50 +0900

    v1.2

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -10,8 +10,11 @@ class Habits extends Component {
         ],
     };
 
+    // Spread Operator : 이 habits 라는 배열 안에 있는 아이템들을 하나하나씩 새로운 배열 안으로 복사해 오는 것을 말함. 
     handleIncrement = (habit) => {
         console.log(`handleIncrement ${habit.name}`);
+        const habits = [...this.state.habits];
+        this.setState(this.state);
     };
 
     handleDecrement = (habit) => {
