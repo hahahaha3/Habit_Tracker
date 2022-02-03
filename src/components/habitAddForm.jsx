860cd7aa8795289e2eb65db4735ba685ed9301a7
hahahaha3 <eksql2689@gmail.com>
2022-02-04 01:02:50 +0900

    v1.3

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class HabitAddForm extends Component {
+    // input 초기화 방법2
+    formRef = React.createRef();
+    inputRef = React.createRef();
+    onSubmit = (event) => {
+        event.preventDefault();
+        //console.log(this.inputRef.current.value);
+        const name = this.inputRef.current.value;
+        name && this.props.onAdd(name);
+        // input창 초기화 방법1
+        //this.inputRef.current.value = '';  
+        this.formRef.current.reset();
+    };
+    render() {
+        return (
+            <form ref={this.formRef} className="add-foorm" onSubmit={this.onSubmit}>
+                <input 
+                    ref={this.inputRef}
+                    type="text" 
+                    className="add-input" 
+                    placeholder="Habit" 
+                />
+                <button className='add-button'>Add</button>
+            </form>
+        );
+    }
+}
+
+export default HabitAddForm;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Habit from './habit';
+import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
     handleIncrement = habit => {
@@ -14,8 +15,14 @@ class Habits extends Component {
         this.props.onDelete(habit);
     };
 
+    handleAdd = habit => {
+        this.props.onAdd(habit);
+    }
+
     render() {
         return (
+            <>
+            <HabitAddForm onAdd={this.handleAdd}/>
             <ul>
                 {
                     this.props.habits.map(habit => 
@@ -26,8 +33,10 @@ class Habits extends Component {
                             onDecrement={this.handleDecrement}
                             onDelete={this.handleDelete}
                         />
-                )}
-            </ul>
+                    )}
+                </ul>
+                <button className="habits-reset" onClick={this.props.onReset}>ResetAll</button>
+            </>
         );
     }
 }
