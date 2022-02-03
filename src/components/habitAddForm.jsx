22569e8ab39fe70e7cea4984474d616424097f04
hahahaha3 <eksql2689@gmail.com>
2022-02-04 01:58:40 +0900

    v1.3

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class HabitAddForm extends Component {
+class HabitAddForm extends PureComponent {
     // input 초기화 방법2
     formRef = React.createRef();
     inputRef = React.createRef();
